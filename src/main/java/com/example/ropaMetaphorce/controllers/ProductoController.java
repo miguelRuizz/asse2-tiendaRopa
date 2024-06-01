48f27dac655b8cd1afaa7446e44fecd3eb53aaeb
profitamoleRuiz <20030935@itcelaya.edu.mx>
2024-06-01 11:18:04 -0600

    Cambio rápido al controlador

--- src/main/java/com/example/ropaMetaphorce/controllers/ProductoController.java
+++ src/main/java/com/example/ropaMetaphorce/controllers/ProductoController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/v1/usuario")
+@RequestMapping("api/v1/producto")
 public class ProductoController {
     private final ProductoService productoService;
     public ProductoController(ProductoService productoService) {
