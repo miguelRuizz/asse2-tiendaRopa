20802ced1542018bc92812a38935ed8897eb31c3
profitamoleRuiz <20030935@itcelaya.edu.mx>
2024-06-01 10:38:17 -0600

    Commit inicial para la creación del proyecto en Spring Boot

---
+++ src/main/java/com/example/ropaMetaphorce/RopaMetaphorceApplication.java
@@ -0,0 +1,13 @@
+package com.example.ropaMetaphorce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RopaMetaphorceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RopaMetaphorceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ropaMetaphorce/RopaMetaphorceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ropaMetaphorce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RopaMetaphorceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
